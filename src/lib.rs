a794450a679dee7e3770ff9433e7fb39f9745a20
olvyko <olvyko@gmail.com>
2021-06-14 05:25:35 +0600

    Moved to new creator version

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,20 @@ use bevy::prelude::*;
 #[creator::creator_main]
 pub fn main() {
     App::build()
-        .add_resource(ClearColor(Color::rgb(0.88, 0.87, 0.86)))
         .add_plugins(DefaultPlugins)
-        .add_startup_system(image.system())
+        .add_startup_system(setup.system())
         .run();
 }
 
-fn image(
-    commands: &mut Commands,
+fn setup(
+    mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let texture_handle = asset_server.load("image.png");
-    commands
-        .spawn(Camera2dBundle::default())
-        .spawn(SpriteBundle {
-            material: materials.add(texture_handle.into()),
-            ..Default::default()
-        });
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(texture_handle.into()),
+        ..Default::default()
+    });
 }
