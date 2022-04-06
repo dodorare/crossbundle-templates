c4223efecb6909f53ff20291b6689bf5c05c5ef7
OffTheHeezay <den_9912@mail.ru>
2022-04-06 19:31:22 +0300

    Upgrade bevy template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::{
+    log::{Level, LogSettings},
+    prelude::*,
+};
+
 fn main() {
-    {{crate_name}}::main();
+    println!("Initialization.");
+    std::thread::sleep(std::time::Duration::from_secs(2));
+    App::new()
+        .insert_resource(LogSettings {
+            level: Level::DEBUG,
+            filter: "wgpu=debug,wgpu_hal=debug,bevy_render=info".to_string(),
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(audio)
+        .add_startup_system(icon)
+        .run();
+}
+
+fn icon(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("branding/icon.png"),
+        ..Default::default()
+    });
+}
+
+fn audio(asset_server: Res<AssetServer>, audio: Res<Audio>) {
+    let music = asset_server.load("sounds/Windless-Slopes.mp3");
+    audio.play(music);
 }
