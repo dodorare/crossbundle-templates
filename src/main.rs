af91a83d658c8b90e90eff4b780d150a7b3f29de
unknown <den_9912@mail.ru>
2022-09-20 16:14:45 +0500

    Update bevy example

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
-use bevy::{
-    log::{Level, LogSettings},
-    prelude::*,
-};
+use bevy::prelude::*;
 
 fn main() {
     println!("Initialization.");
     std::thread::sleep(std::time::Duration::from_secs(2));
     App::new()
-        .insert_resource(LogSettings {
-            level: Level::DEBUG,
-            filter: "wgpu=debug,wgpu_hal=debug,bevy_render=info".to_string(),
-        })
         .add_plugins(DefaultPlugins)
-        .add_startup_system(audio)
+        // .add_startup_system(audio)
         .add_startup_system(icon)
         .run();
 }
 
 fn icon(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(Camera2dBundle::default());
     commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("branding/icon.png"),
+        texture: asset_server.load("branding/launcher.png"),
         ..Default::default()
     });
-}
-
-fn audio(asset_server: Res<AssetServer>, audio: Res<Audio>) {
-    let music = asset_server.load("sounds/Windless-Slopes.mp3");
-    audio.play(music);
 }
